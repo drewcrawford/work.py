437bd3d3186eced22c5e2dad7f11abc838863bde
Drew Crawford <drew@sealedabstract.com>
2011-01-29 12:42:53 -0600

    bumping version

--- work.py
+++ work.py
@@ -249,7 +249,7 @@ fromSpec = ""
 from urllib2 import urlopen
 version_no = urlopen("http://dl.dropbox.com/u/59605/work_autoupdate.txt").read()
 #########################
-WORK_PY_VERSION_NUMBER=5
+WORK_PY_VERSION_NUMBER=6
 #########################
 import re
 if re.search("(?<=WORK_PY_VERSION_NUMBER=)\d+",version_no).group(0) != str(WORK_PY_VERSION_NUMBER):
