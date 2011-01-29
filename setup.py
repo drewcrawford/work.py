2ad524e803a3817fc938152117f120ae727a686d
LesZedCB <chrisblatchley@Mac-Pro-2.local>
2011-01-29 16:15:29 -0600

    added keychain package installer

--- setup.py
+++ setup.py
@@ -8,6 +8,11 @@ except:
 		os.system("tar xfvj BeautifulSoup*")
 		os.system("cd BeautifulSoup-3.2.0/ && python setup.py install")
 		os.system("rm -rf BeautifulSoup*")
+		os.system("wget http://pypi.python.org/packages/source/k/keyring/keyring-0.3.tar.gz")
+		os.system("tar -xzvf keyring-0.3.tar.gz")
+		os.system("cd keyring-0.3")
+		os.system("python setup.py install")
+		os.system("rm -rf keyring-0.3*")
 
 from distutils.core import setup
 setup(name='work.py',version='1.0',
