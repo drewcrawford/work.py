5289607a4a5c27b27c17334b6a18b5e0c081b05b
Drew Crawford <drew@sealedabstract.com>
2011-02-26 16:08:03 -0600

    fixing setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ except:
 		os.system("tar xfvj BeautifulSoup*")
 		os.system("cd BeautifulSoup-3.2.0/ && python setup.py install")
 		os.system("rm -rf BeautifulSoup*")
+try:
+	import keyring
+except:
+	print "It appears you don't have keyring installed.  Want to give that a try? y/n"
+	if (raw_input()=="y"):
+		import os
 		os.system("wget http://pypi.python.org/packages/source/k/keyring/keyring-0.3.tar.gz")
 		os.system("tar -xzvf keyring-0.3.tar.gz")
 		os.system("cd keyring-0.3")
