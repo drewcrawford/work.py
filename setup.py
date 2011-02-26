fe6b89ad4452a8334af11877d77d188392df6610
Drew Crawford <drew@sealedabstract.com>
2011-02-26 16:24:07 -0600

    fixing setup

--- setup.py
+++ setup.py
@@ -14,12 +14,10 @@ except:
 	print "It appears you don't have keyring installed.  Want to give that a try? y/n"
 	if (raw_input()=="y"):
 		import os
-		os.system("wget http://pypi.python.org/packages/source/k/keyring/keyring-0.3.tar.gz")
-		os.system("tar -xzvf keyring-0.3.tar.gz")
-		os.system("cd keyring-0.3")
-		os.system("python setup.py install")
-		os.system("cd ..")
-		os.system("rm -rf keyring-0.3*")
+		os.system("curl -O http://pypi.python.org/packages/source/k/keyring/keyring-0.5.1.tar.gz")
+		os.system("tar -xzvf keyring-0.5.1.tar.gz")
+		os.system("cd keyring-0.5.1 && python setup.py install")
+		os.system("rm -rf keyring-0.5*")
 
 from distutils.core import setup
 setup(name='work.py',version='1.0',
