0c5171bc1b7fbe9e4a09142f49fa19358a978481
Drew Crawford <drew@sealedabstract.com>
2011-04-04 16:35:35 -0500

    version bump

--- work.py
+++ work.py
@@ -254,7 +254,7 @@ fromSpec = ""
 from urllib2 import urlopen
 version_no = urlopen("http://dl.dropbox.com/u/59605/work_autoupdate.txt").read()
 #########################
-WORK_PY_VERSION_NUMBER=7
+WORK_PY_VERSION_NUMBER=8
 #########################
 import re
 if re.search("(?<=WORK_PY_VERSION_NUMBER=)\d+",version_no).group(0) != str(WORK_PY_VERSION_NUMBER):
