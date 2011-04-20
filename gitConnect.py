7c19b75c0dda61bb33655c0b5a4cd6e5b9fd87d2
Chris Blatchley <chris@chrisblatchley.com>
2011-04-19 19:53:53 -0500

    fixes

--- gitConnect.py
+++ gitConnect.py
@@ -75,10 +75,10 @@ class GitConnect:
         if status:
             print "ERROR: merge was unsuccessful."
             # play sounds!
-            getoutput("afplay media/ohno.aiff")
+            commands.getoutput("afplay media/ohno.aiff")
         print "Use 'git push' to ship."
     # play sounds!
-    getoutput("afplay media/hooray.aiff")
+    commands.getoutput("afplay media/hooray.aiff")
     
     #
     # Performs a git pull
