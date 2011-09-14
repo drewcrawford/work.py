9422eaeb3ed507333fd763f63dea385cbcd2bd55
Bion Oren <bion@drewcrawfordapps.com>
2011-09-14 18:42:35 -0500

    CodeLint is now an optional extra tool, not a dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-import os
-os.system("git submodule init")
-os.system("git submodule update")
+try:
+	import lint
+except:
+	print "It appears you don't have CodeLint installed.  Want to give that a try? y/n"
+	if (raw_input()=="y"):
+		import os
+		os.system("curl -L https://github.com/bionoren/CodeLint/tarball/master > lint.tar.gz")
+		os.system("tar xfvj lint.tar.gz")
+		os.system("rm -rf lint.tar.gz")
+		os.system("cd bionoren-CodeLint-* && python setup.py install")
+		os.system("rm -rf bionoren-CodeLint-*")
 
 try:
 	import BeautifulSoup
 except:
 	print "It appears you don't have beautiful soup installed.  Want to give that a try? y/n"
 	if (raw_input()=="y"):
+		import os
 		os.system("curl -O http://www.crummy.com/software/BeautifulSoup/download/3.x/BeautifulSoup-3.2.0.tar.gz")
 		os.system("tar xfvj BeautifulSoup*")
 		os.system("cd BeautifulSoup-3.2.0/ && python setup.py install")
@@ -16,6 +25,7 @@ try:
 except:
 	print "It appears you don't have keyring installed.  Want to give that a try? y/n"
 	if (raw_input()=="y"):
+		import os
 		os.system("curl -O http://pypi.python.org/packages/source/k/keyring/keyring-0.5.1.tar.gz")
 		os.system("tar -xzvf keyring-0.5.1.tar.gz")
 		os.system("cd keyring-0.5.1 && python setup.py install")
