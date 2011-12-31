0eda42c4b9a20d8ba69c272c7409e18eb60f9ff5
Drew Crawford <drew@sealedabstract.com>
2011-12-30 21:15:37 -0600

    Initial Commit

---
+++ JucheLogTests/JucheLogTests.h
@@ -0,0 +1,13 @@
+//
+//  JucheLogTests.h
+//  JucheLogTests
+//
+//  Created by Drew Crawford on 12/30/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JucheLogTests : SenTestCase
+
+@end
---
+++ JucheLogTests/JucheLogTests.m
@@ -0,0 +1,32 @@
+//
+//  JucheLogTests.m
+//  JucheLogTests
+//
+//  Created by Drew Crawford on 12/30/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "JucheLogTests.h"
+
+@implementation JucheLogTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JucheLogTests");
+}
+
+@end
