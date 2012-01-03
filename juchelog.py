cec0d606399cb598b72ed507bce70aa879350e21
Drew Crawford <drew@sealedabstract.com>
2012-01-03 15:44:23 -0600

    epic fix for exception logging

--- juchelog.py
+++ juchelog.py
@@ -7,6 +7,7 @@ import os
 from contextlib import contextmanager
 import threading
 import textwrap
+import sys
 
 def post_async(endpoint,message):
 	r = urllib2.Request(endpoint,data=message,headers={"content-type":"application/json"})
@@ -31,7 +32,16 @@ class LogglyHandler (logging.Handler):
 	def emit(self, record):
 		record = dict(record.__dict__)
 		del record["JUCHE_IS_AWESOME"]
-		post_to_endpoint(self.endpoint, dumps(record))
+		def json_format(obj):
+			import traceback
+			if isinstance(obj,traceback.types.TracebackType):
+				return traceback.extract_tb(obj)
+			if isinstance(obj,type):
+				return repr(obj)
+			if isinstance(obj,Exception):
+				return repr(obj)
+			raise TypeError("Strange type %s" % obj)
+		post_to_endpoint(self.endpoint, dumps(record,default=json_format))
 
 class JucheRecord(logging.LogRecord):
 	def __init__(self,*args,**kwargs):
@@ -48,9 +58,9 @@ class JucheLogger(logging.getLoggerClass()):
 		basicFormatter = IndentFormatter("%(indent)s[%(levelname)s] %(asctime)s %(message)s %(JUCHE_IS_AWESOME)s %(filename)s:%(lineno)s",datefmt='%H:%M:%S')
 		handler = logging.StreamHandler()
 		handler.setFormatter(basicFormatter)
+		handler.setLevel(logging.DEBUG)
 		self.addHandler(handler)
 		#try:
-		LOGGLY_KEY
 		jucheHandler = LogglyHandler(key=LOGGLY_KEY)
 		jucheHandler.setLevel(logging.INFO)
 		self.addHandler(jucheHandler)
@@ -106,16 +116,32 @@ class JucheLogger(logging.getLoggerClass()):
 		yield
 		self.pop()
 
-
+def non_logging_exc():
+	import traceback
+	stack = traceback.extract_stack()
+	arr = []
+	for item in stack:
+		if item[0].find("logging/__init__.py")==-1: arr.append(item)
+		else: break
+	return "".join(traceback.format_list(arr))
 class IndentFormatter(logging.Formatter):
 	def __init__( self, fmt=None, datefmt=None ):
 		logging.Formatter.__init__(self, fmt, datefmt)
+	
 
 	def format( self, rec ):
+		#we need to process exc_info first so that something else doesn't throw (i.e. JUCHE_WRAP not existing)
+		tb = None
+		ext = None
+		if rec.__dict__["exc_info"] and len(rec.__dict__["exc_info"])==3: #exception
+			import traceback
+			tb = "\ntb: " + traceback.format_exc()
+			ext = "\nfull traceback: " + non_logging_exc()
 		dontcare = ["relativeCreated","process","levelno","who","exc_text","indent","name","thread","created","threadName","msecs","pathname",
 		"exc_info","args","levelname","funcName","filename","module","msg","processName","lineno","message","asctime"]
 		rec.indent = '|  '*(rec.indent)
 		out = ""
+
 		for (key,val) in rec.__dict__.iteritems():
 			if key not in dontcare:
 				#key = self.format_sub(key)
@@ -130,14 +156,35 @@ class IndentFormatter(logging.Formatter):
 		out = "%s " % super(IndentFormatter,self).format(rec)
 		out = ("\n"+rec.indent+" ").join(textwrap.wrap(out,width=wrapwidth))
 		del rec.indent
+
+		if tb: #exception
+			import traceback
+			out += tb
+			out += ext
+			rec.__dict__["full_traceback"]=ext
+
 		return out
 
 if __name__=="__main__":
 	__builtins__.LOGGLY_KEY="dbd1f4d5-5c41-4dc7-8803-47666d46e01d"
-	__builtins__.JUCHE_WRAP=60
+	#__builtins__.JUCHE_WRAP=60
 
 logging.setLoggerClass(JucheLogger)
 juche = logging.getLogger("JUCHE")
+def testExcept():
+	try:
+		raise Exception("This is a sample exception!")
+	except Exception as e:
+		import traceback
+		exc_type,exc_value,exc_traceback = sys.exc_info()
+		traceback.print_exception(exc_type, exc_value, exc_traceback,
+                                  limit=3, file=sys.stdout)
+
+        juche.exception(e)
+def testMoreExcept():
+	testExcept()
+
+testMoreExcept()
 if __name__=="__main__":
 	for i in range(0,3):
 		with juche.revolution(i=i):
