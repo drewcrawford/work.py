68ac50b3fd299ce9b19c64f843f7974e8e97f0ab
Drew Crawford <drew@sealedabstract.com>
2012-01-02 19:29:25 -0600

    python-related fixes

--- juchelog.py
+++ juchelog.py
@@ -6,6 +6,7 @@ from urllib import quote_plus
 import os
 from contextlib import contextmanager
 import threading
+import textwrap
 
 def post_async(endpoint,message):
 	r = urllib2.Request(endpoint,data=message,headers={"content-type":"application/json"})
@@ -18,17 +19,19 @@ def post_to_endpoint(endpoint, message):
 	t.start()
 	
 
-class JucheHandler (logging.Handler):
+class LogglyHandler (logging.Handler):
 
 	def __init__(self,  key=''):
-		super(JucheHandler,self).__init__()
+		super(LogglyHandler,self).__init__()
 		self.key = key
 		secure = False
 		protocol = secure and 'https' or 'http'
 		self.endpoint = "%s://%s/inputs/%s" % (protocol, "logs.loggly.com", key)
 
 	def emit(self, record):
-		post_to_endpoint(self.endpoint, dumps(record.__dict__))
+		record = dict(record.__dict__)
+		del record["JUCHE_IS_AWESOME"]
+		post_to_endpoint(self.endpoint, dumps(record))
 
 class JucheRecord(logging.LogRecord):
 	def __init__(self,*args,**kwargs):
@@ -39,46 +42,56 @@ class JucheLogger(logging.getLoggerClass()):
 	def __init__(self,name):
 		super(JucheLogger,self).__init__(name)
 		self.stack = [{"indent":0,"who":os.uname()[1]}]
-		try:
-			JUCHE_KEY
-		except:
-			raise Exception("You need to configure your JUCHE_KEY.  You can do this by setting __builtins__.JUCHE_KEY=<YOUR_KEY> at the top of the script that is currently executing.")
-		jucheHandler = JucheHandler(key=JUCHE_KEY)
+		self.clean_stack = list(self.stack)
 		self.setLevel(logging.DEBUG)
-		jucheHandler.setLevel(logging.INFO)
-		self.addHandler(jucheHandler)
-		basicFormatter = IndentFormatter("[%(levelname)s]%(indent)s%(message)s")
+
+		basicFormatter = IndentFormatter("%(indent)s[%(levelname)s] %(asctime)s %(message)s %(JUCHE_IS_AWESOME)s %(filename)s:%(lineno)s",datefmt='%H:%M:%S')
 		handler = logging.StreamHandler()
 		handler.setFormatter(basicFormatter)
 		self.addHandler(handler)
+		#try:
+		LOGGLY_KEY
+		jucheHandler = LogglyHandler(key=LOGGLY_KEY)
+		jucheHandler.setLevel(logging.INFO)
+		self.addHandler(jucheHandler)
+		#except:
+		#	self.warning("__builtins__.LOGGLY_KEY is not set up, network logging is disabled.")
+		
 	def currentState(self):
 		return self.stack[-1]
 	
 	def push(self):
+		#print "push"
 		self.stack.append(dict(self.currentState()))
-
-	def superPush(self,key,val):
-		self.push()
-		self.indent()
-		self.set(key,val)
+		self.clean_stack.append({})
 
 	def pop(self):
+		#print "pop"
 		self.stack = self.stack[:-1]
-	def set(self,key,val):
-		try:
-			dumps(val)
-		except:
-			val = repr(val)
-		self.stack[-1][key]=val
+		self.clean_stack = self.clean_stack[:-1]
+	def set(self,tuples):
+		#print "setting %s"%tuples
+		for (key,val) in tuples:
+			try:
+				dumps(val)
+			except:
+				val = repr(val)
+			self.stack[-1][key]=val
+			self.clean_stack[-1][key]=val
 
 	def indent(self):
-		self.set("indent",self.currentState()["indent"]+1)
+		self.set([("indent",self.currentState()["indent"]+1)])
 	def dedent(self):
-		self.set("indent",self.currentState()["indent"]-1)
+		self.set([("indent",self.currentState()["indent"]-1)])
+	def get_clean_stack_context(self,howMany):
+		retdict = {}
+		for i in range(1,howMany+1):
+			if i < len(self.clean_stack): retdict.update(self.clean_stack[-1*i])
+		return retdict
 	
 	def makeRecord(self, name, level, fn, lno, msg, args, exc_info, func=None, extra=None):
 		record =  JucheRecord(name, level, fn, lno, msg, args, exc_info, func)
-		for (key,val) in self.currentState().iteritems():
+		for (key,val) in self.get_clean_stack_context(1).iteritems():
 			record.__setattr__(key,val)
 		record.indent = self.currentState()["indent"]
 		return record
@@ -89,19 +102,51 @@ class JucheLogger(logging.getLoggerClass()):
 		self.push()
 		self.indent()
 		for (key,val) in kwargs.iteritems():
-			self.set(key,val)
+			self.set([(key,val)])
 		yield
 		self.pop()
 
 
 class IndentFormatter(logging.Formatter):
-    def __init__( self, fmt=None, datefmt=None ):
-        logging.Formatter.__init__(self, fmt, datefmt)
-    def format( self, rec ):
-        rec.indent = '  '*(rec.indent)
-        out = logging.Formatter.format(self, rec)
-        del rec.indent
-        return out
+	def __init__( self, fmt=None, datefmt=None ):
+		logging.Formatter.__init__(self, fmt, datefmt)
+
+	def format( self, rec ):
+		dontcare = ["relativeCreated","process","levelno","who","exc_text","indent","name","thread","created","threadName","msecs","pathname",
+		"exc_info","args","levelname","funcName","filename","module","msg","processName","lineno","message","asctime"]
+		rec.indent = '|  '*(rec.indent)
+		out = ""
+		for (key,val) in rec.__dict__.iteritems():
+			if key not in dontcare:
+				#key = self.format_sub(key)
+				#val = self.format_sub(val)
+				out += "%s=%s " % (key,val)
+
+		rec.JUCHE_IS_AWESOME=out
+		try:
+			wrapwidth = JUCHE_WRAP
+		except:
+			wrapwidth = 9999999
+		out = "%s " % super(IndentFormatter,self).format(rec)
+		out = ("\n"+rec.indent+" ").join(textwrap.wrap(out,width=wrapwidth))
+		del rec.indent
+		return out
+
+if __name__=="__main__":
+	__builtins__.LOGGLY_KEY="dbd1f4d5-5c41-4dc7-8803-47666d46e01d"
+	__builtins__.JUCHE_WRAP=60
 
 logging.setLoggerClass(JucheLogger)
 juche = logging.getLogger("JUCHE")
+if __name__=="__main__":
+	for i in range(0,3):
+		with juche.revolution(i=i):
+			juche.info("My awesome loop")
+	
+
+	for i in range(0,3):
+		with juche.revolution(i=i,eternal_president="kim-il-sun"):
+			juche.info("Outer loop!")
+			for j in range(0,2):
+				with juche.revolution(j=j):
+					juche.info("Inner loop!")
