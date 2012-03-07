b78144162e434c740781fee12f05087b35433c95
Drew Crawford <drew@sealedabstract.com>
2012-03-07 17:09:38 -0600

    too many js

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except:
 	if (raw_input()=="y"):
 		import os
 		os.system("curl -L https://github.com/bionoren/CodeLint/tarball/master > lint.tar.gz")
-		os.system("tar xfvj lint.tar.gz")
+		os.system("tar xfv lint.tar.gz")
 		os.system("rm -rf lint.tar.gz")
 		os.system("cd bionoren-CodeLint-* && python setup.py install")
 		os.system("rm -rf bionoren-CodeLint-*")
@@ -17,7 +17,7 @@ except:
 	if (raw_input()=="y"):
 		import os
 		os.system("curl -O http://www.crummy.com/software/BeautifulSoup/bs3/download//3.x/BeautifulSoup-3.2.0.tar.gz")
-		os.system("tar xfvj BeautifulSoup*")
+		os.system("tar xfv BeautifulSoup*")
 		os.system("cd BeautifulSoup-3.2.0/ && python setup.py install")
 		os.system("rm -rf BeautifulSoup*")
 try:
@@ -27,7 +27,7 @@ except:
 	if (raw_input()=="y"):
 		import os
 		os.system("curl -O http://pypi.python.org/packages/source/k/keyring/keyring-0.5.1.tar.gz")
-		os.system("tar -xzvf keyring-0.5.1.tar.gz")
+		os.system("tar -xvf keyring-0.5.1.tar.gz")
 		os.system("cd keyring-0.5.1 && python setup.py install")
 		os.system("rm -rf keyring-0.5*")
 
