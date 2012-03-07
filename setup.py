53576581138b327197858c1157abf56fdf651858
Drew Crawford <drew@sealedabstract.com>
2012-03-07 17:07:19 -0600

    beautiful soup url

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except:
 	print "It appears you don't have beautiful soup installed.  Want to give that a try? y/n"
 	if (raw_input()=="y"):
 		import os
-		os.system("curl -O http://www.crummy.com/software/BeautifulSoup/download/3.x/BeautifulSoup-3.2.0.tar.gz")
+		os.system("curl -O http://www.crummy.com/software/BeautifulSoup/bs3/download//3.x/BeautifulSoup-3.2.0.tar.gz")
 		os.system("tar xfvj BeautifulSoup*")
 		os.system("cd BeautifulSoup-3.2.0/ && python setup.py install")
 		os.system("rm -rf BeautifulSoup*")
